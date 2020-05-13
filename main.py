51ec582f529912b8fce6587bfb655d8ef7c0ca8f
Nikhil Jha <hi@nikhiljha.com>
2020-05-13 08:47:39 -0700

    add shorturl endpoint

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
+
+from ocflib.misc.shorturls import get_shorturl
+from ocflib.misc.shorturls import get_connection
+
+app = FastAPI()
+
+@app.get("/shorturl/{slug}")
+def short_url(slug: str):
+    if slug:
+        with get_connection() as ctx:
+            target = get_shorturl(ctx, slug)
+            if target:
+                return RedirectResponse(target)
+    raise HTTPException(status_code=404, detail="short url not found")
+
